f24578b063be151cc5be3974b1bb05d538582021
easonchiu0327 <easonchiu0327@gmail.com>
2025-08-17 22:28:15 -0600

    Fix adding scriptmanager in site mobile master

--- web-crawling-findingjobs/Site.Mobile.Master.designer.cs
+++ web-crawling-findingjobs/Site.Mobile.Master.designer.cs
@@ -7,11 +7,13 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace web_crawling_findingjobs {
-    
-    
-    public partial class Site_Mobile {
-        
+namespace web_crawling_findingjobs
+{
+
+
+    public partial class Site_Mobile
+    {
+
         /// <summary>
         /// HeadContent control.
         /// </summary>
@@ -20,7 +22,7 @@ namespace web_crawling_findingjobs {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
-        
+
         /// <summary>
         /// form1 control.
         /// </summary>
@@ -29,7 +31,16 @@ namespace web_crawling_findingjobs {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
+
+        /// <summary>
+        /// ScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+
         /// <summary>
         /// FeaturedContent control.
         /// </summary>
@@ -38,7 +49,7 @@ namespace web_crawling_findingjobs {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder FeaturedContent;
-        
+
         /// <summary>
         /// MainContent control.
         /// </summary>
